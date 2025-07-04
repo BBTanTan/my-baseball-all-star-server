91228bb935fac41d044d49ae45951297091a4be4
gmuz1c <songjin1127@naver.com>
2025-07-04 15:34:53 +0900

    chore: init project

---
+++ my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/MyBaseballAllStarApplication.java
@@ -0,0 +1,13 @@
+package bbTan.my_baseball_all_star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBaseballAllStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBaseballAllStarApplication.class, args);
+	}
+
+}
---
+++ my-baseball-all-star/src/test/java/bbTan/my_baseball_all_star/MyBaseballAllStarApplicationTests.java
@@ -0,0 +1,13 @@
+package bbTan.my_baseball_all_star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBaseballAllStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
