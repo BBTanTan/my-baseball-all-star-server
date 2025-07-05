a6c343f52aa4b663ea8399ed19ed300534553778
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-07-06 02:59:34 +0900

    feat: createdAt 필드 추가

--- my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/domain/PlayResult.java
+++ my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/domain/PlayResult.java
@@ -1,6 +1,7 @@
 package bbTan.my_baseball_all_star.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,13 +9,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class PlayResult {
 
@@ -31,6 +36,9 @@ public class PlayResult {
 
     private Integer opposingTeam;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -42,12 +50,13 @@ public class PlayResult {
         PlayResult that = (PlayResult) o;
         return Objects.equals(id, that.id) && Objects.equals(team, that.team)
                 && Objects.equals(opposingTeamName, that.opposingTeamName) && Objects.equals(
-                ownTeamScore, that.ownTeamScore) && Objects.equals(opposingTeam, that.opposingTeam);
+                ownTeamScore, that.ownTeamScore) && Objects.equals(opposingTeam, that.opposingTeam)
+                && Objects.equals(createdAt, that.createdAt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, team, opposingTeamName, ownTeamScore, opposingTeam);
+        return Objects.hash(id, team, opposingTeamName, ownTeamScore, opposingTeam, createdAt);
     }
 
     @Override
@@ -58,6 +67,7 @@ public class PlayResult {
                 ", opposingTeamName='" + opposingTeamName + '\'' +
                 ", ownTeamScore=" + ownTeamScore +
                 ", opposingTeam=" + opposingTeam +
+                ", createdAt=" + createdAt +
                 '}';
     }
 }
--- my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/domain/Team.java
+++ my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/domain/Team.java
@@ -1,18 +1,23 @@
 package bbTan.my_baseball_all_star.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Team {
 
@@ -26,6 +31,9 @@ public class Team {
 
     private Long winPlayCount;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -37,12 +45,12 @@ public class Team {
         Team team = (Team) o;
         return Objects.equals(id, team.id) && Objects.equals(name, team.name)
                 && Objects.equals(totalPlayCount, team.totalPlayCount) && Objects.equals(winPlayCount,
-                team.winPlayCount);
+                team.winPlayCount) && Objects.equals(createdAt, team.createdAt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, totalPlayCount, winPlayCount);
+        return Objects.hash(id, name, totalPlayCount, winPlayCount, createdAt);
     }
 
     @Override
@@ -52,6 +60,7 @@ public class Team {
                 ", name='" + name + '\'' +
                 ", totalPlayCount=" + totalPlayCount +
                 ", winPlayCount=" + winPlayCount +
+                ", createdAt=" + createdAt +
                 '}';
     }
 }
