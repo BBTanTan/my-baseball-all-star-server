72e86941d20a7676f8f531eca3046111f3b5fc63
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-07-08 16:55:18 +0900

    feat: TeamRepository 구현

---
+++ my-baseball-all-star/src/test/java/bbTan/my_baseball_all_star/fixture/TeamFixture.java
@@ -0,0 +1,10 @@
+package bbTan.my_baseball_all_star.fixture;
+
+import bbTan.my_baseball_all_star.domain.Team;
+
+public class TeamFixture {
+
+    public static Team TEAM1() {
+        return new Team("팀1");
+    }
+}
