e390ecd26379050575dac932959dd08df913b036
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-07-09 15:10:04 +0900

    style: TODO 주석 추가

--- my-baseball-all-star/src/test/resources/data-test.sql
+++ my-baseball-all-star/src/test/resources/data-test.sql
@@ -1,19 +1,31 @@
-INSERT INTO player (name, club, position, score, date_of_birth, profile_url) VALUES
-                                                                                 ('원태인', 'SAMSUNG_LIONS', 'STARTING_PITCHER', 54.66, '2000-04-06', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/69446.jpg'),
-                                                                                 ('최승용', 'DOOSAN_BEARS', 'STARTING_PITCHER', 35.16, '2001-05-11', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/51264.jpg'),
-                                                                                 ('소형준', 'KT_WIZ', 'STARTING_PITCHER', 60.92, '2001-09-16', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/50030.jpg'),
-                                                                                 ('김광현', 'SSG_LANDERS', 'STARTING_PITCHER', 45.55, '1988-07-22', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/77829.jpg'),
-                                                                                 ('박세웅', 'LOTTE_GIANTS', 'STARTING_PITCHER', 43.21, '1995-11-30', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/64021.jpg'),
-                                                                                 ('네일', 'KIA_TIGERS', 'STARTING_PITCHER', 62.53, '1993-02-08', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/54640.jpg'),
-                                                                                 ('임찬규', 'LG_TWINS', 'STARTING_PITCHER', 53.66, '1992-11-20', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/61101.jpg'),
-                                                                                 ('폰세', 'HANWHA_EAGLES', 'STARTING_PITCHER', 75.35, '1994-04-25', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/55730.jpg'),
-                                                                                 ('라일리', 'NC_DINOS', 'STARTING_PITCHER', 66.41, '1996-07-09', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/55903.jpg'),
-                                                                                 ('로젠버그', 'KIWOOM_HEROES', 'STARTING_PITCHER', 55.0, '1995-07-09', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/55322.jpg');
+INSERT INTO player (name, club, position, score, date_of_birth, profile_url)
+VALUES ('원태인', 'SAMSUNG_LIONS', 'STARTING_PITCHER', 54.66, '2000-04-06',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/69446.jpg'),
+       ('최승용', 'DOOSAN_BEARS', 'STARTING_PITCHER', 35.16, '2001-05-11',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/51264.jpg'),
+       ('소형준', 'KT_WIZ', 'STARTING_PITCHER', 60.92, '2001-09-16',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/50030.jpg'),
+       ('김광현', 'SSG_LANDERS', 'STARTING_PITCHER', 45.55, '1988-07-22',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/77829.jpg'),
+       ('박세웅', 'LOTTE_GIANTS', 'STARTING_PITCHER', 43.21, '1995-11-30',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/64021.jpg'),
+       ('네일', 'KIA_TIGERS', 'STARTING_PITCHER', 62.53, '1993-02-08',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/54640.jpg'),
+       ('임찬규', 'LG_TWINS', 'STARTING_PITCHER', 53.66, '1992-11-20',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/61101.jpg'),
+       ('폰세', 'HANWHA_EAGLES', 'STARTING_PITCHER', 75.35, '1994-04-25',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/55730.jpg'),
+       ('라일리', 'NC_DINOS', 'STARTING_PITCHER', 66.41, '1996-07-09',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/55903.jpg'),
+       ('로젠버그', 'KIWOOM_HEROES', 'STARTING_PITCHER', 55.0, '1995-07-09',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/55322.jpg');
 
 
-INSERT INTO player (name, club, position, score, date_of_birth, profile_url) VALUES
-                                                                                 ('김영웅', 'SAMSUNG_LIONS', 'THIRD_BASE', 0, '2003-08-24', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/52430.jpg'),
-                                                                                 ('임종성', 'DOOSAN_BEARS', 'THIRD_BASE', 0, '2005-03-03', 'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/54236.jpg');
+INSERT INTO player (name, club, position, score, date_of_birth, profile_url)
+VALUES ('김영웅', 'SAMSUNG_LIONS', 'THIRD_BASE', 0, '2003-08-24',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/52430.jpg'),
+       ('임종성', 'DOOSAN_BEARS', 'THIRD_BASE', 0, '2005-03-03',
+        'https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/54236.jpg');
 INSERT INTO player_choice_count (player_id, count, version)
 VALUES (1, 0, 0),
        (2, 0, 0),
@@ -32,18 +44,17 @@ INSERT INTO team (name, total_play_count, win_play_count, created_at)
 VALUES ('TestTeam', 0, 0, NOW());
 
 INSERT INTO team_player (team_id, player_id)
-VALUES
-    (1, 1),
-    (1, 2),
-    (1, 3),
-    (1, 4),
-    (1, 5),
-    (1, 6),
-    (1, 7),
-    (1, 8),
-    (1, 9),
-    (1, 10),
-    (1, 11),
-    (1, 12);
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7),
+       (1, 8),
+       (1, 9),
+       (1, 10),
+       (1, 11),
+       (1, 12);
 
 
