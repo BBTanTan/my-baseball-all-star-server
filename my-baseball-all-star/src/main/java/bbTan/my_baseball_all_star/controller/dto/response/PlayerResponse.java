3f6a3894fa640d26a9ce4f2d275d321fe500fff1
gmuz1c <songjin1127@naver.com>
2025-07-12 01:34:15 +0900

    [BE] 선수 정보 생년월일 추가

--- my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/controller/dto/response/PlayerResponse.java
+++ my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/controller/dto/response/PlayerResponse.java
@@ -2,11 +2,12 @@ package bbTan.my_baseball_all_star.controller.dto.response;
 
 import bbTan.my_baseball_all_star.domain.Player;
 
-public record PlayerResponse (long id, String name,
+public record PlayerResponse (long id, String name, String dateOfBirth,
                               String club, String position,
                               Double score, String profileUrl) {
     public static PlayerResponse fromEntity(Player player) {
-        return new PlayerResponse(player.getId(), player.getName(), player.getClub().getName(),
-                player.getPosition().getName(), player.getScore(), player.getProfileUrl());
+        return new PlayerResponse(player.getId(), player.getName(), player.getDateOfBirth().toString(),
+                player.getClub().getName(), player.getPosition().getName(),
+                player.getScore(), player.getProfileUrl());
     }
 }
