ed4972a753ef0e615902786951e249cb283f98a0
gmuz1c <songjin1127@naver.com>
2025-07-14 15:38:14 +0900

    [BE] DB 병목 현상 개선 (#66)
    * feat(AllStarFacadeService): 기존에 존재하는 팀은 선택 횟수 반영하지 않도록 수정
    
    * feat(PlayerScoreCrawlerService): 점수 반영 트랜잭션 레포지토리로 이전
    
    * feat(PlayerRepository): 트랜잭션 설정 추가

--- my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/repository/PlayerRepository.java
+++ my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/repository/PlayerRepository.java
@@ -1,6 +1,7 @@
 package bbTan.my_baseball_all_star.repository;
 
 import bbTan.my_baseball_all_star.domain.Player;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,5 +11,6 @@ public interface PlayerRepository extends JpaRepository<Player, Long> {
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE Player p SET p.score = :score WHERE p.id = :id")
+    @Transactional
     int updateScoreById(@Param("id") Long id, @Param("score") Double score);
 }
