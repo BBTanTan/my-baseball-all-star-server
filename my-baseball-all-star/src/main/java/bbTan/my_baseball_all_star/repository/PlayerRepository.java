dc900cd71521ee7e05121c0a277dd4d7c8298738
gmuz1c <songjin1127@naver.com>
2025-07-14 16:40:38 +0900

    [BE] 개별 쿼리를 bulk로 처리하도록 변경 (#68)

--- my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/repository/PlayerRepository.java
+++ my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/repository/PlayerRepository.java
@@ -1,16 +1,7 @@
 package bbTan.my_baseball_all_star.repository;
 
 import bbTan.my_baseball_all_star.domain.Player;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("UPDATE Player p SET p.score = :score WHERE p.id = :id")
-    @Transactional
-    int updateScoreById(@Param("id") Long id, @Param("score") Double score);
 }
