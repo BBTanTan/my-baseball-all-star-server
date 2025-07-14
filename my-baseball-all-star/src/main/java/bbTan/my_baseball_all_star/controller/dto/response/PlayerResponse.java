7c22d669b007943ac984484debdd6d7452de9b45
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-07-14 21:51:28 +0900

    feat: 친구 팀 조회 응답값 변경

--- my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/controller/dto/response/PlayerResponse.java
+++ my-baseball-all-star/src/main/java/bbTan/my_baseball_all_star/controller/dto/response/PlayerResponse.java
@@ -3,11 +3,9 @@ package bbTan.my_baseball_all_star.controller.dto.response;
 import bbTan.my_baseball_all_star.domain.Player;
 
 public record PlayerResponse (long id, String name, String dateOfBirth,
-                              String club, String position,
-                              Double score, String profileUrl) {
+                              String club, String position, String profileUrl) {
     public static PlayerResponse fromEntity(Player player) {
         return new PlayerResponse(player.getId(), player.getName(), player.getDateOfBirth().toString(),
-                player.getClub().getName(), player.getPosition().getName(),
-                player.getScore(), player.getProfileUrl());
+                player.getClub().getName(), player.getPosition().getName(), player.getProfileUrl());
     }
 }
